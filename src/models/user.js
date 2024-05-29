cc843c6e4e5513bf85225fbff89f5e14ed80de55
Muhammad Rafif Tri Risqullah <zeonkunix@gmail.com>
2024-05-29 20:33:06 +0700

    move unused files

--- src/models/user.js
+++ src/models/user.js
@@ -1,38 +1,34 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    static associate(models) {
-      // define association here
-    }
-  }
-  User.init({
-    userName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: true // Allow null for users authenticated with Google
-    },
-    googleId: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      unique: true
-    }
-  }, {
-    sequelize,
-    modelName: 'User',
-    tableName: 'users', // specify the table name
-    timestamps: true // this is default, you can omit this if you like
-  });
-  return User;
-};
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: true, // Allow null for users authenticated with Google
+  },
+  googleId: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    unique: true,
+  },
+}, {
+  tableName: 'users',
+  timestamps: true,
+});
+
+module.exports = User;
--- src/server.js
+++
@@ -1,5 +0,0 @@
-const app = require('./app');
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
-});
