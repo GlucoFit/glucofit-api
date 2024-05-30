35d6faa3482341b95f5009233bff62d55c1cf538
Muhammad Rafif Tri Risqullah <zeonkunix@gmail.com>
2024-05-30 12:38:33 +0700

    feat: add profile API

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,11 +3,16 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const authenticateJWT = require('../middleware/authenticateJWT');
 
-// User routes
 router.post('/users', authenticateJWT, userController.createUser);
-router.get('/users', authenticateJWT, userController.getAllUsers);
-router.get('/users/:id', authenticateJWT, userController.getUserById);
-router.put('/users/:id', authenticateJWT, userController.updateUser);
-router.delete('/users/:id', authenticateJWT, userController.deleteUser);
+// router.get('/users', authenticateJWT, userController.getAllUsers);
+
+router.get('/users/me', authenticateJWT, userController.getUserMe);
+router.put('/users/me/username', authenticateJWT, userController.updateUsername);
+router.put('/users/me/email', authenticateJWT, userController.updateEmail);
+router.put('/users/me/password', authenticateJWT, userController.updatePassword);
+router.delete('/users/me', authenticateJWT, userController.deleteUserMe);
+// router.get('/users/:id', authenticateJWT, userController.getUserById);
+// router.put('/users/:id', authenticateJWT, userController.updateUser);
+// router.delete('/users/:id', authenticateJWT, userController.deleteUser);
 
 module.exports = router;
--- src/services/userService.js
+++ src/services/userService.js
@@ -4,30 +4,70 @@ const createUser = async (userData) => {
   return User.create(userData);
 };
 
-const getAllUsers = async () => {
-  return User.findAll();
+const getUserMe = async (userId) => {
+  return User.findByPk(userId);
 };
 
-const getUserById = async (id) => {
-  return User.findByPk(id);
-};
-
-const updateUser = async (id, userData) => {
-  const user = await User.findByPk(id);
+const updateUsername = async (userId, newUsername) => {
+  const user = await User.findByPk(userId);
+  if (user.googleId) throw new Error('User is authenticated with google, cannot update data');
   if (!user) throw new Error('User not found');
-  return user.update(userData);
+  user.userName = newUsername;
+  return user.save();
 };
 
-const deleteUser = async (id) => {
-  const user = await User.findByPk(id);
+const updateEmail = async (userId, newEmail) => {
+  const user = await User.findByPk(userId);
+  if (user.googleId) throw new Error('User is authenticated with google, cannot update data');
+  if (!user) throw new Error('User not found');
+  user.email = newEmail;
+  return user.save();
+};
+
+const updatePassword = async (userId, newPassword) => {
+  const user = await User.findByPk(userId);
+  if (user.googleId) throw new Error('User is authenticated with google, cannot update data');
+  if (!user) throw new Error('User not found');
+  user.password = newPassword;
+  return user.save();
+};
+
+const deleteUserMe = async (userId) => {
+  const user = await User.findByPk(userId);
   if (!user) throw new Error('User not found');
   return user.destroy();
 };
 
+// const getAllUsers = async () => {
+//   return User.findAll();
+// };
+
+// const getUserById = async (id) => {
+//   return User.findByPk(id);
+// };
+
+// const updateUser = async (id, userData) => {
+//   const user = await User.findByPk(id);
+//   if (!user) throw new Error('User not found');
+//   return user.update(userData);
+// };
+
+// const deleteUser = async (id) => {
+//   const user = await User.findByPk(id);
+//   if (!user) throw new Error('User not found');
+//   return user.destroy();
+// };
+
 module.exports = {
   createUser,
-  getAllUsers,
-  getUserById,
-  updateUser,
-  deleteUser,
+  createUser,
+  getUserMe,
+  updateUsername,
+  updateEmail,
+  updatePassword,
+  deleteUserMe,
+  // getAllUsers,
+  // getUserById,
+  // updateUser,
+  // deleteUser,
 };
