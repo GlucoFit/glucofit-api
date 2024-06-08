99ae2f19e2fc2b84613f14bac060c23ac66a6071
Muhammad Rafif Tri Risqullah <zeonkunix@gmail.com>
2024-06-08 21:35:25 +0700

    urgent minor change for scan api

--- src/controllers/scanController.js
+++ src/controllers/scanController.js
@@ -5,17 +5,20 @@ const uploadImageAndSave = async (req, res) => {
         if (!req.file) {
             return res.status(400).json({ error: 'No file uploaded.' });
         }
-        // console.log(req);
+        
         const userId = req.user.id;
         const { objectName } = req.body;
+        const { objectSugar } = req.body;
 
         if (!objectName) {
             return res.status(400).json({ error: 'Object name and object sugar are required.' });
         }
-
+        
         const objectImageUrl = await scanService.uploadImage(req.file, 'user-image', userId);
         // console.log('controller: saving image from controller king');
-        const savedImage = await scanService.saveImage(objectImageUrl, objectName, userId);
+        //========CHANGE THIS OBJECT SUGAR TO PREDICT FUNCTION LATER
+        // const objectSugar = 0;
+        const savedImage = await scanService.saveImage(objectImageUrl, objectName, objectSugar, userId);
         res.status(201).json({ message: 'Image uploaded and saved successfully.' , data: savedImage});
     } catch (error) {
         res.status(500).json({ error: error.message });
