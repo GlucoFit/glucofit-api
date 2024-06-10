6bdcc66a4c691e92bde485ce4d6618f2bedc0b11
Muhammad Rafif Tri Risqullah <zeonkunix@gmail.com>
2024-06-10 19:17:16 +0700

    minor fix on env calling

--- src/routes/foodRoute.js
+++ src/routes/foodRoute.js
@@ -7,4 +7,7 @@ const authenticateJWT = require('../middleware/authenticateJWT');
  * API for Food
  */
 
-//getFoodBy
+// text editor nya ngedit -> dia bakal manggil PreData -> kalau udah fix , pencet searc -> manggil FixData
+//getFoodbyFoodRecipeNamePreData
+//getFoodByFoodRecipeNameFixData -> parameter /food/:name -> controller : req.params.name -> dicocokin sama database food where : name
+//getFoodByFoodPrimaryKeyId -> /food/:id -> controller : req.params.id
